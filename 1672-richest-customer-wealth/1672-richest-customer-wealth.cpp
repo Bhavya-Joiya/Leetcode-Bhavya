955126dce7cd6da26fdc39df059d5777ac2c19ff
Bhavya-Joiya <bhavyajoiya8200@gmail.com>
2026-07-30 15:09:24 +0530

    Time: 0 ms (100%), Space: 11.2 MB (98.82%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int n = accounts.size();
+        int maxWealth = 0;
+
+        for(int i = 0; i < n; i++) {
+            int sum = 0;
+
+            for(int j = 0; j < accounts[i].size(); j++) {
+                sum += accounts[i][j];
+            }
+
+            if(sum > maxWealth) {
+                maxWealth = sum;
+            }
+        }
+
+        return maxWealth;
+    }
+};
