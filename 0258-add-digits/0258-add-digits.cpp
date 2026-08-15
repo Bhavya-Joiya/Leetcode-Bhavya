430fb1baa51e993f53b723df28f2634e05d17773
Bhavya-Joiya <bhavyajoiya8200@gmail.com>
2026-08-15 17:19:07 +0530

    Time: 0 ms (100%), Space: 8.7 MB (46.11%) - LeetHub

---
+++ 0258-add-digits/0258-add-digits.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        int sum = 0;
+        int a = 0;
+        while(num >= 10){
+            while(num > 0){
+                a = num % 10;
+                sum += a;
+                num /= 10;
+            }
+            num = sum;
+            sum = 0;
+        }
+        return num;
+    }
+};
